a101bb7d4666f56e5e0a1df4d87c384fa4fc8dd4
Shrey Patidar <shrey.p@beehyv.com>
2023-08-09 11:39:43 +0530

    Dummy MR.

---
+++ src/main/java/com/iemr/hwc/fhir/DummyClass.java
@@ -0,0 +1,23 @@
+package com.iemr.hwc.fhir;
+
+import java.util.Map; //Unused imports
+import java.util.Random;
+
+public class DummyClass {
+    int MyVariable = 42; //Wrong naming convention
+    String s = "check"; //unused variable
+
+    public void dummyMethod(){
+        if (MyVariable == 42){
+        System.out.println("dsfvds");
+        //Wrong indentation
+            }
+
+                        }
+
+    public void emptyMethod(int Param1, int Param2){ //Wrong param naming
+        if(){
+                //missing closing bracket
+
+    }
+}
