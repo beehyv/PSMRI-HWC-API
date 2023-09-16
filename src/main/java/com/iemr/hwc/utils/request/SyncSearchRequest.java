eaa4268d32ea0c69c2fa1212a632a1a7d04ec1a7
Shrey Patidar <shrey.p@beehyv.com>
2023-09-16 16:05:02 +0530

    Implemented pagination for ben flow records down sync API.

--- src/main/java/com/iemr/hwc/utils/request/SyncSearchRequest.java
+++ src/main/java/com/iemr/hwc/utils/request/SyncSearchRequest.java
@@ -8,4 +8,6 @@ public class SyncSearchRequest {
     private String lastSyncDate;
     private Long lastModifiedDate;
     private List<Integer> villageID;
+    private Integer pageNo;
+    private Integer pageSize;
 }
